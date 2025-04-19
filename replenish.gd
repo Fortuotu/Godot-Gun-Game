b5b92fbc1b1b6f18ddc2b505784001b6206aba79
Fortuotu <103213729+Fortuotu@users.noreply.github.com>
2025-04-19 19:26:35 +0300

    Fixed timer for replenish powerup

--- replenish.gd
+++ replenish.gd
@@ -1,19 +1,26 @@
 extends Area2D
 
+const REPLENISH_COUNTDOWN = 5.0 
+
 func replenish():
 	Player.gun_ammo = Player.GUN_INITIAL_AMMO
-	disable_node(self, 5.0)
-
+	
+	disable_node(self)
+	$Timer.start(REPLENISH_COUNTDOWN)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		replenish()
 
-func disable_node(node: Node, duration: float):
+func disable_node(node: Node):
 	node.set_process(false)
 	node.set_physics_process(false)
 	node.visible = false  
-	await get_tree().create_timer(duration).timeout
+
+func enable_node(node: Node):
 	node.set_process(true)
 	node.set_physics_process(true)
 	node.visible = true
+
+func _on_timer_timeout() -> void:
+	enable_node(self)
