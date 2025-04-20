f2e76db4f77e0ca2bc367e2aa90a7c0652837c15
Fortuotu <103213729+Fortuotu@users.noreply.github.com>
2025-04-20 12:28:33 +0300

    Bullet overlay

--- scenes/content/level.gd
+++ scenes/content/level.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 var pause_menu_scene = preload("res://scenes/UI/pause_menu.tscn") 
+var bullets_label_scene = preload("res://scenes/UI/bullet_overlay.tscn")
 
 func _ready():
 	_load_ui_layer()
 
 func _load_ui_layer():
 	var canvas_layer = CanvasLayer.new()
+	canvas_layer.add_child(bullets_label_scene.instantiate())
 	canvas_layer.add_child(pause_menu_scene.instantiate())
 	add_child(canvas_layer)
