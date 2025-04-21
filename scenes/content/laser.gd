cec3de8ca95da21a1d12ac426cce93675000fa91
Fortuotu <103213729+Fortuotu@users.noreply.github.com>
2025-04-21 21:26:25 +0300

    laser

---
+++ scenes/content/laser.gd
@@ -0,0 +1,58 @@
+extends RayCast2D
+
+@onready var activated_ray_size: float = $Line2D.width
+
+func _manage_cast_particle_effects():
+	$CollisionParticles.global_position = global_position
+	$CastParticles.global_rotation = global_rotation
+
+func _manage_collision_particle_effects():
+	if (!is_colliding()):
+		$CollisionParticles.emitting = false
+		return
+	else:
+		$CollisionParticles.emitting = true
+	
+	var normal = get_collision_normal()
+	
+	$CollisionParticles.global_position = get_collision_point()
+	$CollisionParticles.global_rotation = normal.angle()
+
+func _manage_beam_particle_effects():
+	$BeamParticles.position = target_position * 0.5
+	$BeamParticles.emission_rect_extents.y = target_position.length() * 0.5
+
+func _check_player_collision():
+	if is_colliding():
+		var body = get_collider()
+		if body.is_in_group("player"):
+			Global.get_level().respawn_player()
+
+func _ready() -> void:
+	stop_ray()
+
+func _physics_process(delta: float) -> void:
+	var hit_position = get_collision_point() if is_colliding() else Vector2.ZERO
+	
+	_manage_cast_particle_effects()
+	_manage_collision_particle_effects()
+	_manage_beam_particle_effects()
+	
+	_check_player_collision()
+	
+	$Line2D.points[1] = to_local(hit_position)
+
+func start_ray():
+	create_tween().tween_property($Line2D, "width", activated_ray_size, 0.1)
+	set_physics_process(true)
+	
+	$CastParticles.emitting = true
+	$BeamParticles.emitting = true
+
+func stop_ray():
+	create_tween().tween_property($Line2D, "width", 0.0, 0.1)
+	set_physics_process(false)
+	
+	$CastParticles.emitting = false
+	$CollisionParticles.emitting = false
+	$BeamParticles.emitting = false
---
+++ scenes/content/laser_shooter.gd
@@ -0,0 +1,24 @@
+class_name LaserShooter extends StaticBody2D
+
+var laser_active: bool = false
+
+const LASER_INTERVAL = 0.5
+const LASER_DURATION = 1.0
+
+func _configure_laser():
+	$Laser.target_position *= 1000
+
+func _ready() -> void:
+	_configure_laser()
+	
+	$Timer.start(LASER_INTERVAL)
+
+func _on_timer_timeout() -> void:
+	if laser_active:
+		$Laser.stop_ray()
+		$Timer.start(LASER_INTERVAL)
+	else:
+		$Laser.start_ray()
+		$Timer.start(LASER_DURATION)
+	
+	laser_active = !laser_active
